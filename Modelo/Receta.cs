279ad541e86233e1707160ec87679c9427837beb
Lucas Macias <lucas12macias@gmail.com>
2019-04-04 16:26:42 -0300

    Project Modelo Added New project of models with 2 models added

---
+++ Modelo/Receta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Modelo
+{
+    public class Receta
+    {
+        public int Id { get; set; }
+
+        public int Nombre { get; set; }
+
+        public List<Ingrediente> Ingredientes { get; set; }
+
+        public string Instrucciones { get; set; }
+
+        public List<Utensilios> Utensilios { get; set; }
+    }
+}
