1f4e297d04f589e7addf0474469c664c547fd395
Lucas Macias <lucas12macias@gmail.com>
2019-04-04 15:58:30 -0300

    Modelo created With Properties

---
+++ EjercicioRepositorios/Modelo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace EjercicioRepositorios
+{
+    public class Modelo
+    {
+        public int Peso { get; set; }
+
+        public string Talle { get; set; }
+    }
+}
